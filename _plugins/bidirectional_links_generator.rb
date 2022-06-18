9b0751dafe613dd633c7cce4baa562a98ff82e5c
Srikanth Sastry <srikanthsastry@gmail.com>
2022-06-18 10:01:47 -0400

    Fixing the backlink generation to include post_url liquid tags (#9)

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -6,9 +6,9 @@ class BidirectionalLinksGenerator < Jekyll::Generator
 
     posts.each do |current_post|
       posts_linking_to_current_post = posts.filter do |e|
-        e.content.include?(current_post.url)
+        # Check if the url is included, or if the post filename is included via {%post_url file-name-whithout-md-ext %}
+        e.content.include?(current_post.url) || e.content.include?(File.basename(current_post.path, '.md'))
       end
-
       current_post.data['backlinks'] = posts_linking_to_current_post
     end
   end
